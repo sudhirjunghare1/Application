a0020b15f413bd39a9470b90cc8674a535119f76
sudhirjunghare1 <40244735+sudhirjunghare1@users.noreply.github.com>
2018-06-16 07:22:27 +0530

    Results the App

---
+++ Application.java
@@ -0,0 +1,11 @@
+//Main.java
+public class Main{
+ public static void main(String[]args)
+ //create main class obj
+ Main m1=new Main();
+ displayGreeting();
+ m1.display();
+}
+private static void displayGreeting(){
+System.out.println("Hello Happy Sales People!");
+System.out.println("This App Shows Sales Data!");
