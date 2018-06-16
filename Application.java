d3d7ac1c2d856857253dd51d36268698433ee0c2
sudhirjunghare1 <40244735+sudhirjunghare1@users.noreply.github.com>
2018-06-16 07:28:49 +0530

    Wishiing Msg

--- Application.java
+++ Application.java
@@ -9,3 +9,7 @@ public class Main{
 private static void displayGreeting(){
 System.out.println("Hello Happy Sales People!");
 System.out.println("This App Shows Sales Data!");
+ 
+ system.out.println("Thanks For Information");
+}
+}
